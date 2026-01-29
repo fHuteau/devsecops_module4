2e05db4c8fdf68c3b27399b32875d8e0b1c9dc44
florian <florian.huteau@proton.me>
2026-01-29 09:56:48 +0100

    add secrets

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   apiKey: "AKIAIOSFODNN7EXAMPLE",
   secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
   dbPassword: "P@ssw0rd"
-  githubToken: "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  githubToken: "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  adminPass: "P@ssw0rd"
 };
 module.exports = config;
 
