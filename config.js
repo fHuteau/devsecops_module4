2ddce516c0e8ec1124b28b2b8c125110fc4360fa
florian <florian.huteau@proton.me>
2026-01-29 09:47:29 +0100

    add env

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   apiKey: "AKIAIOSFODNN7EXAMPLE",
   secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-  dbPassword: "amlzkamzlekamzlekamz",
+  dbPassword: "P@ssw0rd"
   githubToken: "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 module.exports = config;
