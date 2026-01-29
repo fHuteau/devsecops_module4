00cf22b01d908288a9ad693612c1234815d6c1be
florian <florian.huteau@proton.me>
2026-01-29 09:40:37 +0100

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AKDZIDFKZDKFIDPZKFK",
+  secretKey: "jslkjlksjdfoziejskdfm",
+  dbPassword: "amlzkamzlekamzlekamz",
+  githubToken: "ghp_sjhdkfjshdkfjsdkfjshdkjfsdk"
+};
+module.exports = config;
+
